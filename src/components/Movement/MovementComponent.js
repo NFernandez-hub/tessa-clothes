b5ff6adb09fae74a1a99b1389034a01820a16b9e
geronimo <gerolpz01@gmail.com>
2020-11-15 03:31:32 -0300

    primeros cambios

---
+++ src/components/Movement/MovementComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MovementComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Stock/StockComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StockComponent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
